201973458a0311cdefdb725622e401dde726e35c
root <root@localhost.localdomain>
2018-09-08 15:26:54 +0800

    change style.

--- settings.php
+++ settings.php
@@ -5,14 +5,20 @@
 	$tx_color = '#00ff00';	/* TX graph color, default is #00ff00 */
 	$rx_color = '#ff0000';	/* RX graph color, default is #ff0000 */
 	$theme = 'default';     /* Default CSS theme to use (one of: 'default', 'nox') */
+//	$theme = 'nox';     /* Default CSS theme to use (one of: 'default', 'nox') */
 	$precision = 2;		    /* Number of decimal digits to display in table, default is 2 (e.g. 2 = 0.00, 3 = 0.000, etc...) */
 	//date_default_timezone_set('Europe/Berlin'); // depending on your php settings you might want to explicitly set this to your TZ
+	date_default_timezone_set('Asia/Shanghai'); // depending on your php settings you might want to explicitly set this to your TZ
 	$date_format = array(   /* date formats shown in tables and sidebar, see php's date() for reference */
 		'hours' => 'H:00',
-		'days'  => 'D, d.m.Y',
-		'months'=> 'M Y',
-		'top10' => 'd.m.Y',
-		'uptime'=> 'd.m.Y, H:i'
+//		'days'  => 'D, d.m.Y',
+		'days'  => 'Y-m-d D',
+//		'months'=> 'M Y',
+		'months'=> 'Y-m',
+//		'top10' => 'd.m.Y',
+		'top10' => 'Y-m-d',
+//		'uptime'=> 'd.m.Y, H:i'
+		'uptime'=> 'Y-m-d H:i'
 	);
 	$enabled_dropdowns = array(
 		'interface' => true,
